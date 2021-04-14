4f2ba56aa779e8322525e6bf0076408924198c42
Reinis Danne <rei4dan@gmail.com>
2021-04-14 09:47:19 +0300

    tests: Get connection from Db module
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- tests/unit/MySQLTransactionRepositoryTest.php
+++ tests/unit/MySQLTransactionRepositoryTest.php
@@ -7,53 +7,45 @@ use InvestmentTool\Config;
 use InvestmentTool\Entities\Transaction;
 use InvestmentTool\Repositories\MySQLTransactionRepository;
 use PDO;
-use PDOException;
 use UnitTester;
 
 class MySQLTransactionRepositoryTest extends Unit
 {
     private const CONFIG_FILENAME = '.env.test';
 
-    private static ?MySQLTransactionRepository $repository;
-    private static ?PDO $connection;
-
     protected UnitTester $tester;
 
-    public static function setUpBeforeClass(): void
+    private ?MySQLTransactionRepository $repository;
+    private ?PDO $connection;
+
+    public function _before(): void
     {
         $config = new Config(self::CONFIG_FILENAME);
 
-        self::$repository = new MySQLTransactionRepository($config);
+        $this->repository = new MySQLTransactionRepository($config);
+        $this->connection = $this->getModule('Db')->dbhs['default'];
 
-        $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-            PDO::ATTR_EMULATE_PREPARES => false,
-        ];
+        $this->resetTestDBTables();
+    }
 
-        try {
-            self::$connection = new PDO(
-                $config->getDsn(),
-                $config->getDBUsername(),
-                $config->getDBPassword(),
-                $options
-            );
-        } catch (PDOException $e) {
-            throw new PDOException($e->getMessage(), (int)$e->getCode());
-        }
+    public function _after(): void
+    {
+        $this->resetTestDBTables();
+    }
 
-        // Reset tables in the test database
+    private function resetTestDBTables(): void
+    {
         $sql = "drop table if exists `investments_test`.`transaction_log`;";
-        self::$connection->exec($sql);
+        $this->connection->exec($sql);
 
         $sql = "create table `investments_test`.`transaction_log` like `investments`.`transaction_log`;";
-        self::$connection->exec($sql);
+        $this->connection->exec($sql);
     }
 
     public function testAddTransaction(): void
     {
         $transaction = new Transaction('XYZ', 42, 5);
-        self::$repository->add($transaction);
+        $this->repository->add($transaction);
 
         $this->tester->seeInDatabase('transaction_log', ['symbol' => 'XYZ', 'quote' => 42, 'amount' => 5]);
     }
