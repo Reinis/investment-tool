e2d214ce53ba27fe4b76d4eb284734ec51b55aed
Reinis Danne <rei4dan@gmail.com>
2021-04-16 01:22:30 +0300

    search: Compact form for buying
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- app/ValidationRules/BuyPostValidationRules.php
+++ app/ValidationRules/BuyPostValidationRules.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 
 namespace InvestmentTool\ValidationRules;
 
@@ -16,13 +18,29 @@ class BuyPostValidationRules
     {
         $rules = new Rules();
 
-        $exists = new Condition('exists', function (string $value) {
-            return '' !== trim($value);
-        });
+        $exists = new Condition(
+            'exists',
+            function (string $value) {
+                return '' !== trim($value);
+            }
+        );
+
+        $number = new Condition(
+            'number',
+            function (string $value) {
+                return false !== filter_var($value, FILTER_VALIDATE_FLOAT);
+            }
+        );
+
+        $pozitive = new Condition(
+            'pozitive',
+            function (string $value) {
+                return 0 < filter_var($value, FILTER_VALIDATE_FLOAT);
+            }
+        );
 
         $rules->add(new Rule('symbol', new Conditions($exists)));
-        $rules->add(new Rule('quote', new Conditions($exists)));
-        $rules->add(new Rule('amount', new Conditions($exists)));
+        $rules->add(new Rule('amount', new Conditions($exists, $number, $pozitive)));
 
         return $rules;
     }
